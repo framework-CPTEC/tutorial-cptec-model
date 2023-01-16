b928112e8356b01dc8d8dcf8639b791b435bb159
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2023-01-16 14:39:10 -0300

    Trocando nomes do template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CPTEC-Model'
+copyright = '2023, Framework CPTEC'
+author = 'Framework CPTEC'
 
 release = '0.1'
 version = '0.1.0'
