fb12287d9e3321fd68f670bc42a31994f25a1e34
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2023-01-18 11:28:00 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'nbsphinx',
+    'sphinx.ext.nbsphinx',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
